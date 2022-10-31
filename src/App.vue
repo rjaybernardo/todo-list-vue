33db44a9010e29b82dd764a9ee79d3ca353da85d
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-10-31 12:12:36 +0800

    setup local storage

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,37 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
 
-<template><h1>hello world</h1></template>
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+// filters todo list in ascending
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+// watch changes and setup local storage
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+// save the name to localStorage
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up,
+        <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
+</template>
